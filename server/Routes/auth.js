86cecec73ad01e1fb8dd0950224be86bd157fd70
Ayo <talktojmcvibes@gmail.com>
2024-05-18 16:08:26 +0100

    encrypt user password using bcrypt

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const userModel = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 router.get('/', (req, res)=>{
     res.send({message: 'you hit the api route'}).json()
@@ -12,22 +13,32 @@ router.post('/signup', (req, res)=>{
     if (!firstName || !lastName || !email || !password){
         //res.send({message: "Include firstName, lastName, email & password in the body"})
         console.log('Signup route no params')
-        res.sendStatus(401)
+        res.status(401).json({message: "Cant Sign up user. Include firstName, lastName, email & password in the request body"})
     } else{
-        console.log({
-            firstName,
-            lastName,
-            email,
-            password
-        });
-        const newUser = new userModel({
-            firstName,
-            lastName,
-            email,
-            password
+        // console.log({
+        //     firstName,
+        //     lastName,
+        //     email,
+        //     password
+        // });
+        const SALT = parseInt(process.env.SALT)
+        bcrypt.hash(password, SALT, (err, newPassword)=>{
+            try{
+                const newUser = new userModel({
+                    firstName,
+                    lastName,
+                    email,
+                    password: newPassword
+                })
+                newUser.save()
+                res.status(200).json({message: "User created successfully"})
+            } catch(err){
+                console.log(`Error creating user ${err}`)
+            }
+           
         })
-        newUser.save()
-        res.sendStatus(200)
+
+        
     }
     
 })
