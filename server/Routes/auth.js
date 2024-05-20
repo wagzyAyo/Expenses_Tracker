4b46c40450e25a94089607dca209020eb4360e12
Ayo <talktojmcvibes@gmail.com>
2024-05-20 11:14:18 +0100

    add logout route to dstroy the cookie

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -74,4 +74,14 @@ router.post('/signup',  (req, res)=>{
     
 })
 
+router.post('/logout', (req, res)=>{
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    });
+
+    res.status(200).json({message: 'User logged out'})
+
+})
+
 module.exports = router
