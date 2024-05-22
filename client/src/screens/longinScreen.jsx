a1a6ec135b856aac11ec4d3e4613cb398895c3be
Ayo <talktojmcvibes@gmail.com>
2024-05-22 11:57:04 +0100

    update login & signup UI

--- client/src/screens/longinScreen.jsx
+++ client/src/screens/longinScreen.jsx
@@ -1,11 +1,80 @@
+import {useState} from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import IconButton from '@mui/material/IconButton';
+import {StyleSheet, css} from 'aphrodite'
+import logo from '../assets/Logo.png'
 
+const LonginScreen = () => {
+    const [showPassword, setShowPassword] = useState(false);
 
-const longinScreen = () => {
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div>
-      <p>login</p>
+    <div className={css(styles.flexDisplay)}>
+      <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 2, width: '350px', display: 'grid'},
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <img src={logo} alt={logo} className={css(styles.logo)}/>
+      <TextField id="outlined-basic" label="email" variant="outlined" name='email' />
+      <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+    </Box>
     </div>
   )
 }
 
-export default longinScreen
+
+const styles = StyleSheet.create(
+    {
+        flexDisplay: {
+        width: '600px',
+        height: 'auto',
+        padding: '2em',
+        border: '2px solid grey',
+        borderRadius: '20px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: "center",
+        margin: '20px auto'
+    },
+    logo: {
+        width: '100px',
+        margin: '20px auto'
+    }
+}
+)
+export default LonginScreen
--- client/src/screens/signupScreen.jsx
+++ client/src/screens/signupScreen.jsx
@@ -1,11 +1,60 @@
+import {useState} from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import IconButton from '@mui/material/IconButton';
 
+const SignupScreen = () => {
+    const [showPassword, setShowPassword] = useState(false);
 
-const signupScreen = () => {
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
-      <p>signup</p>
+      <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '350px' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="firstName" variant="outlined" name='firstName' />
+      <TextField id="outlined-basic" label="lastName" variant="outlined" name='lastName' />
+      <TextField id="outlined-basic" label="email" variant="outlined" name='email' />
+      <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+      <TextField id="outlined-basic" label="confirm paswword" variant="outlined" name='confirmPassword' type='password'/>
+    </Box>
     </div>
   )
 }
 
-export default signupScreen
+export default SignupScreen
