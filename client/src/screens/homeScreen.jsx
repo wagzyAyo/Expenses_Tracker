85af00488656768cd94eeec17fd6165da9b2cdee
AmponsahJ99 <amjust99@gmail.com>
2024-05-26 17:21:30 +0000

    homescreen file updated successfully

--- client/src/screens/homeScreen.jsx
+++ client/src/screens/homeScreen.jsx
@@ -1,11 +1,20 @@
-import { greeting } from "../utils.js/utilities"
+import Nav from "../components/nav";
+import Intro from "../components/intro";
+import Footer from "../components/footer"
+
 
 const homeScreen = () => {
   return (
-    <div>
-       <h1 className="text-center text-yellow-500">{greeting} John</h1>
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-grow">
+        <Nav />
+        <Intro />
+       </main>
+       <Footer />
     </div>
   )
 }
 
+
 export default homeScreen
+
