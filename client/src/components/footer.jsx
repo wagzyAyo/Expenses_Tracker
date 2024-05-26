ff01bb8c3743e5050c6138e0c97257c7c25512e9
AmponsahJ99 <amjust99@gmail.com>
2024-05-26 17:18:09 +0000

    react components files updated successfully

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,12 +1,17 @@
 import { year } from "../utils.js/utilities"
 
+
 const Footer = () => {
   return (
-    <div className="text-center relative end-0">
-      <p>copy right &copy; {year}</p>
+    <div className="text-center">
+      <p>copyright &copy; {year}</p>
     </div>
   )
 }
 
+
 export default Footer
+
+
+
 
--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -1,13 +1,35 @@
 import icon from '/icon.svg'
 import Button from '@mui/material/Button';
+import {StyleSheet, css} from "aphrodite"
+
 
 const Nav = () => {
   return (
-    <div className='basis-0 justify-between align-center'>
+    <div className={css(styles.nav)}>
+      <div>
       <img src={icon} alt="logo" />
+      </div>
+      <div>
       <Button variant="outlined">Logout</Button>
+      </div>
+     
     </div>
   )
 }
 
+
+const styles = StyleSheet.create({
+  nav: {
+    position: 'relative',
+    top: '1em',
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '3px 10%'
+  }
+})
+
+
 export default Nav
+
+
+
