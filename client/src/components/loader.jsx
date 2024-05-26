20eb0494a06482ec8a54f3aff2ebebb4d2889bb7
AmponsahJ99 <amjust99@gmail.com>
2024-05-26 12:37:02 +0000

    react components updated

---
+++ client/src/components/loader.jsx
@@ -0,0 +1,13 @@
+import {Spinner} from 
+'@material-tailwind/react'
+
+const loader = () => {
+return (
+ <div>
+ <spinner
+className = 'h-12 w-12 text-gray-900/50'
+ />
+</div>
+)
+}
+export default loader
