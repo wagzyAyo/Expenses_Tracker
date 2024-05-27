e53a00c8188c86fcab1325d01d15f864b3eb7e83
AmponsahJ99 <amjust99@gmail.com>
2024-05-27 18:28:43 +0000

    update utils with net spend

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -2,7 +2,9 @@ const currentDate = new Date();
 const time = currentDate.getHours();
 const year = currentDate.getFullYear();
 
+
 let greeting = "";
+
 
 if (time < 12) {
     greeting = "Good morning";
@@ -11,5 +13,41 @@ if (time < 12) {
 } else {
     greeting = "Good evening";
 }
+
+
+
+
+export const toTitleCase = (str) => {
+    if (!str) return '';
+    return str.replace(/\w\S*/g, (txt) => {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  };
+
+
+  export const netSpend = (data) => {
+    let total = 0;
+
+
+    if(Array.isArray(data)){
+      data.forEach(element => {
+        total += element.amount
+      })
+    } else{
+      console.warn('Error data is not an array or equal to null')
+    }
+   
+    return total
+  }
+
+
+  export const toMoney = (amount) =>{
+    if (isNaN(amount)){
+      console.log(`Amount is null`)
+    }
+    const formattedAmount = amount.toFixed(0)
+    return formattedAmount.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
 
 export { greeting, year };
