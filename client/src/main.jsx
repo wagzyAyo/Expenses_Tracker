fbe996ac801b2d3e52a324d0a19055aff3d47ff1
AmponsahJ99 <amjust99@gmail.com>
2024-05-29 19:05:43 +0000

    update main.jsx to serve homescreen on the index page and add private route to dashboard, addnew and update routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { 
-  RouterProvider, 
-  Route, 
+import {
+  RouterProvider,
+  Route,
   createBrowserRouter,
   createRoutesFromElements} from "react-router-dom"
 import App from './App.jsx';
@@ -12,25 +12,33 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import "./index.css"
+import "./index.css";
+import HomeScreen from './screens/HomeScreen.jsx';
 import UserDashboard from './screens/UserDashboard.jsx'
 import LoginScreen from './screens/loginScreen.jsx'
 import SignupScreen from './screens/signupScreen.jsx';
 import AddNewExpense from './screens/AddNewExpense.jsx';
 import UpdateExpense from './screens/UpdateExpense.jsx';
+import PrivateRoute from './components/privateRoute.jsx';
+
+
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-      <Route index={true} path='/' element={<LoginScreen />}></Route>
+      <Route index={true} path='/' element={<HomeScreen />}></Route>
+      <Route  path='/login' element={<LoginScreen />}></Route>
       <Route path='/signup' element={<SignupScreen />}></Route>
-      <Route path='/dashboard' element={<UserDashboard />}></Route>
-      <Route path='/addnew' element={<AddNewExpense />}></Route>
-      <Route path='/update/:id' element={<UpdateExpense />}></Route>
+      <Route path='' element={<PrivateRoute/>}>
+        <Route path='/dashboard' element={<UserDashboard />}></Route>
+        <Route path='/addnew' element={<AddNewExpense />}></Route>
+        <Route path='/update/:id' element={<UpdateExpense />}></Route>
+        </Route>
     </Route>
   )
 )
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
