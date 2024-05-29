33a312227927b7b339c6e7753df02edabe674f23
AmponsahJ99 <amjust99@gmail.com>
2024-05-29 19:12:01 +0000

    Add function to get Total expenses for every category in array

---
+++ client/src/utils/componentsUtils.js
@@ -0,0 +1,10 @@
+export const calculateSummary = (expenses) => {
+    return expenses.reduce((acc, expense) => {
+      if (!acc[expense.category]) {
+        acc[expense.category] = 0;
+      }
+      acc[expense.category] += expense.amount;
+      return acc;
+    }, {});
+  };
+
