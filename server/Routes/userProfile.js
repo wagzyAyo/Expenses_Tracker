c1feadbbd7d404adbb97f90908efc1ef51b0358a
Ayo <talktojmcvibes@gmail.com>
2024-07-05 16:24:02 +0100

    add route to update budget

--- server/Routes/userProfile.js
+++ server/Routes/userProfile.js
@@ -32,6 +32,36 @@ route.post('/', authenticateToken, async (req, res)=>{
         return res.status(500).json({message: "Internal server error"})
     }
     
+});
+
+
+route.put('/:id/update', authenticateToken, async(req, res)=>{
+    const user = req.user
+    const {id} = req.params;
+    const {category, amount} = req.body;
+
+    if(!category || !amount){
+        return res.status(401).json({message: "please provide all required fields category and amount"})
+    }
+     try {
+        const userBudget = user.Budget
+        let budgetFound = false;
+        userBudget.forEach(budget => {
+            if(budget._id.toString() === id){
+                budget.category = category;
+                budget.amount = amount;
+                budgetFound = true
+            }
+            
+        });
+        if(!budgetFound){
+            await user.save()
+            return res.status(201).json({message: "Budget updated suuccessfully"})
+        }
+     } catch (err) {
+        console.log(`Error updating budget ${err}`)
+        return res.status(500).json({message: "Internal server error"})
+     }
 })
 
 module.exports = route;
