60f2ac4c62f6ac3d0047be3b0b3ed0d685c136be
Ayo <talktojmcvibes@gmail.com>
2024-07-06 21:29:21 +0100

    update app.js with profile Route

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const authRouter = require('./Routes/auth')
-const mongoose = require('mongoose')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const authRouter = require('./Routes/auth');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+require('dotenv').config();
 const dataRouter = require('./Routes/data');
-const budgetRouter = require('./Routes/budget')
+const budgetRouter = require('./Routes/budget');
+const profileRouter = require("./Routes/profile");
 
 const port = process.env.PORT || 5000
 const app = express()
@@ -21,6 +22,7 @@ app.use(cors({
 app.use('/api', authRouter)
 app.use('/api/data', dataRouter)
 app.use('/api/budget', budgetRouter)
+app.use('/api/updateprofile', profileRouter)
 
 //connect to database
 const mongoURI = process.env.MongoURI
