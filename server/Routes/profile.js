6074fdd9cc220feb8243cc8ed6bc907dd35bacde
Ayo <talktojmcvibes@gmail.com>
2024-07-07 16:27:59 +0100

    implement update password api

--- server/Routes/profile.js
+++ server/Routes/profile.js
@@ -1,20 +1,29 @@
 const express = require("express");
 const route = express.Router();
 const authenticateToken = require("../utils/authToken")
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
+const userModel = require("../models/user")
 
 
 //update profile
 //prefix  api/updateprofile
 route.put('/', authenticateToken, async (req, res)=>{
-    const user = req.user;
+    
     const {firstName, lastName, email, password} = req.body;
+    const user = await userModel.findOne({email})
     
 
    
     if (!firstName || !lastName || !email || !password){
         return res.status(400).json({message: "Error updating profile firstName, lastName, email & password fields are required"})
     }
+
+    if (!user || !user.password) {
+        return res.status(500).json({ message: "User data not found or incomplete" });
+    }
+
+    
+
     try {
         const comparedPass = await bcrypt.compare(password, user.password)
 
@@ -27,11 +36,39 @@ route.put('/', authenticateToken, async (req, res)=>{
         await user.save()
         return res.status(200).json({message: "Profile updated successfully"})
     } catch (err) {
+        console.error("Error updating profile:", err);
         return res.status(500).json({message: "Internal server error"})
     }
    
 
 
+});
+
+route.put('/password', authenticateToken, async (req, res)=>{
+    const {email, oldPassword, newPassword} = req.body;
+    const user = await userModel.findOne({email})
+
+    if (!email || !oldPassword || !newPassword){
+        res.status(400).json({message: "email, old password and new password fields are required"});
+    }
+
+    try {
+        const userPass = await bcrypt.compare(oldPassword, user.password);
+        const SALT = parseInt(process.env.SALT)
+        if(!user){
+            return res.status(400).json({message: "No user found with this email"})
+        }
+        if(!userPass){
+            return res.status(401).json({message: "Password does not match"})
+        }
+        user.password = await bcrypt.hash(newPassword, SALT);
+        await user.save();
+        return res.status(200).json({message: "Password updated"});
+
+    } catch (err) {
+        console.log(`Error updating user password ${err}`);
+        return res.status(500).json({message: "Internal server error"})
+    }
 })
 
 module.exports = route
