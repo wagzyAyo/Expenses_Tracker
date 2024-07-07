77a9b705385daaa0aa44a65756631290bf374a8b
Ayo <talktojmcvibes@gmail.com>
2024-07-07 15:50:08 +0100

    debug update profile api

--- server/Routes/profile.js
+++ server/Routes/profile.js
@@ -9,20 +9,23 @@ const bcrypt = require("bcryptjs")
 route.put('/', authenticateToken, async (req, res)=>{
     const user = req.user;
     const {firstName, lastName, email, password} = req.body;
-    const comparedPass = bcrypt.compare(password, user.password)
+    
 
-    if (user.password !== comparedPass){
-        return res.status(400).json({message: "Password do not match"})
-    }
+   
     if (!firstName || !lastName || !email || !password){
-        return res.status(401).json({message: "Error updating profile firstName, lastName, email & password fields are required"})
+        return res.status(400).json({message: "Error updating profile firstName, lastName, email & password fields are required"})
     }
     try {
+        const comparedPass = await bcrypt.compare(password, user.password)
+
+        if (!comparedPass){
+            return res.status(400).json({message: "Password does not match"})
+        }
         user.firstName = firstName;
         user.lastName = lastName;
         user.email = email;
         await user.save()
-        return res.status(201).json({message: "Profile updated successfully"})
+        return res.status(200).json({message: "Profile updated successfully"})
     } catch (err) {
         return res.status(500).json({message: "Internal server error"})
     }
