f36174868f9b87a18dc0760bee04dfa00bee4db2
Ayo <talktojmcvibes@gmail.com>
2025-02-08 20:11:31 +0100

    remove token in browser

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -56,6 +56,7 @@ const Nav = () => {
         credentials: "include"
       })
       if(response.status === 200){
+        document.cookie = "jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
         navigate('/')
       }else{
         window.location.reload()
